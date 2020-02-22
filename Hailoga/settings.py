cc69eb7c8894ed4b8477a5105be43f67f1d7c4af
natyrix <natrix277@gmail.com>
2020-02-22 17:09:38 +0300

    Setting modefied

--- Hailoga/settings.py
+++ Hailoga/settings.py
@@ -16,8 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$12evuo-xjn15r0*rmk8-7&adsk%2%rebyty+v2vtq07^$++&+'
